0341e4855ef4ecd10845a370a3def224f22bf068
mooroom <61869246+mooroom@users.noreply.github.com>
2020-12-12 12:33:16 +0900

    image, url filter half completed

--- proxy.py
+++ proxy.py
@@ -2,24 +2,27 @@ import os, sys, threading, socket, signal
 
 MAX_DATA_RECV = 999999
 DEBUG = True
-BLOCKED = ["yonsei"]
-# clients = []
+clients= []
+no = 1
+thread_num = 0
+url_filter = 'X'
+image_filter = 'X'
 
 def main():
 
     if(len(sys.argv) < 2):
-        print("No port given, using: 8080")
-        port = 8080
+        print('Please enter port number.')
+        sys.exit(1)
     else:
         port = int(sys.argv[1])
+        print('Starting proxy server on port {}'.format(port))
 
     host = 'localhost'
-    print("Proxy Server Running on {} : {}".format(host, port))
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
-    s.listen(10)
+    s.listen(20)
 
 
     while True:
@@ -27,77 +30,59 @@ def main():
             conn, client_addr = s.accept()
 
         except KeyboardInterrupt:
-            for client in clients:
-                client.close()
-            print('\nexit')
             break
-        
-        # clients.append(conn)
+
+        clients.append(conn)
         t = threading.Thread(target=proxy_thread, args=(conn, client_addr))
         t.setDaemon(True)
         t.start()
 
+    print('\n=======================')
+    print('Proxy server terminated.')
     s.close()
-
-def printout(type, request, address):
-    colornum = 0
-    if("Block" in type or "Blacklist" in type):
-        colornum = 91
-    elif("Request" in type):
-        colornum = 92
-    elif("Reset" in type):
-        colornum = 93
-
-    print("\033[ {} m {} \t {} \t {} \033[0m".format(colornum, address[0], type, request))
+    sys.exit(1)
 
 def proxy_thread(conn, client_addr):
     request = conn.recv(MAX_DATA_RECV)
 
-    ########
-    #no_img
-    # splited = request.split(b'\r\n')
+    global thread_num
+    global no
 
-    # newop = []
-    # index = 0
-    # for i in splited:
-    #     decoded = i.decode()
-    #     if('Accept: ' in decoded):
-    #         options = decoded.split()[1]
-    #         option = options.split(',')
-            
-    #         for e in option:
-    #             if not('image' in e):
-    #                 newop.append(e)
-    #         break
-    #     index = index + 1
+    global image_filter
+    global url_filter
 
-    # newops = ','.join(newop)
-    # origin = 'Accept: ' + newops
-    # encode_origin = origin.encode()
-    # splited[index] = encode_origin
+    thread_num += 1
 
-    # req_no_img = b'\r\n'.join(splited)
-    #########
+    # print("-------------------")
+    # print("REQUEST: ")
+    # print(request)
+    # print("-------------------")
 
-    print("CONNECTION: ")
-    print(conn)
-    print("-------------------")
-    print("REQUEST: ")
-    # reqsplited = request.split()
-    # for i in reqsplited:
-    #     print(i.decode('utf-8'))
-    print(request)
-    print("-------------------")
+    first_line = request.decode().split('\r\n')[0]
 
-    first_line = request.decode().split('\n')[0]
+    try:
+        url = first_line.split('GET ')[1]
+    except:
+        print('URL not found')
+        sys.exit(1)
 
-    url = first_line.split(' ')[1]
+    #######################
+    #url filter
+    if 'yonsei' in url:
+        url_filter = 'O'
+    else:
+        url_filter = 'X'
 
-    for i in range(0, len(BLOCKED)):
-        if BLOCKED[i] in url:
-            printout("Blacklisted", first_line, client_addr)
-            conn.close()
-            sys.exit(1)
+    #image filter
+    if '?image_off' in url:
+        image_filter = 'O'
+        url.split('?image_off')[0]
+    elif 'image_on' in url:
+        image_filter = 'X'
+        url.split('?image_on')[0]
+    else:
+        image_filter = 'O'
+    #######################
 
     http_pos = url.find("://")
     if(http_pos == -1):
@@ -120,49 +105,102 @@ def proxy_thread(conn, client_addr):
         port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
         webserver = temp[:port_pos] 
 
-    print("WEBSERVER:")
-    print(webserver)
-    print("-------------------")
+    # print("WEBSERVER:")
+    # print(webserver)
+    # print("-------------------")
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+    print('-----------------------------------------------')
+    print('{}  [Conn:    {}/  {}]'.format(no, thread_num, len(clients)))
+    print('[ {} ] URL filter | [ {} ] Image filter\n'.format(url_filter, image_filter))
+    print('[CLI connected to {}:{}]'.format(client_addr[0], client_addr[1]))
+    print('[CLI ==> PRX --- SRV]')
+
+    no += 1
+
+    #get request message
+    req_messages = request.decode().split('\r\n')
+    get_message = req_messages[0]
+    user_agent_message = ''
+
+    for i in req_messages:
+        if 'User-Agent' in i:
+            user_agent_message = i.split('User-Agent: ')[1]
+    ####################
+
+    print('\t> {}'.format(get_message))
+    print('\t> {}'.format(user_agent_message))
+
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+    if url_filter == 'O':
+        webserver = 'www.linuxhowtos.org'
+
     s.connect((webserver, port))
+    print('[SRV connected to {}:{}]'.format(webserver, port))
+
     s.sendall(request)
+    print('[CLI --- PRX ==> SRV]')
+    print('\t> {}'.format(get_message))
+    print('\t> {}'.format(user_agent_message))
 
     while 1:
         data = s.recv(MAX_DATA_RECV)
-        print("DATA:")
+        # print("DATA:")
         
         img_flag = False
         splited = data.split(b'\r\n\r\n')
         header = splited[0]
-        print('===header=====')
-        print(header)
-        print('==========')
+        # print('===header=====')
+        # print(header)
+        # print('==========')
+
+        res_status = ''
+        res_type = ''
+        res_size = ''
+
+        header_exits = False
 
         try:
             decoded_header = header.decode()
-            if 'image' in decoded_header:
-                print('=========')
-                print('img detected!')
-                print('===========')
-                img_flag = True
-        except:
-            pass       
+            if image_filter == 'O':
+                if 'image/' in decoded_header:
+                    img_flag = True
 
-        # for i in splited:
-        #     print(i)
-        # print("-------------------")
+            splited_header = decoded_header.split('\r\n')
+            res_status = splited_header[0].split('HTTP/1.1 ')[1]
+
+            for i in splited_header:
+                if 'Content-Type' in i:
+                    res_type = i.split('Content-Type: ')[1]
+                if 'Content-Length' in i:
+                    res_size = i.split('Content-Length: ')[1]
+
+            print('[CLI --- PRX <== SRV]')
+            print('\t> {}'.format(res_status)) 
+            print('\t> {} {}bytes'.format(res_type, res_size))
+
+            header_exits = True
+        except:
+            pass 
+
+        
 
         if(len(data) > 0 and img_flag == False) :
+            if header_exits:
+                print('[CLI <== PRX --- SRV]')
+                print('\t> {}'.format(res_status)) 
+                print('\t> {} {}bytes'.format(res_type, res_size)) 
             conn.send(data)
-        elif socket.error or KeyboardInterrupt:
+        else:
             break
 
     conn.close()
-    # s.close()
-    
-    # print("---------Exit---------")
-    # sys.exit(1)
+    clients.remove(conn)
+    print('[CLI disconnected]')
+
+    s.close()
+    print('[SRV disconnected]')
 
 
 if __name__ == '__main__':
